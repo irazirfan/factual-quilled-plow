48c18582f734c37c45e7c4dbdf0b36bc73a098a7
Glitch (factual-quilled-plow) <none>
2020-04-07 12:23:11 +0000

    🚝👜 Updated with Glitch

--- myApp.js
+++ myApp.js
@@ -2,6 +2,12 @@ var express = require("express");
 var app = express();
 //console.log("Hello World");
 
+app.use(function middleware(req, res, next) {
+  var string = req.method + " " + req.path + " - " + req.ip;
+  console.log(string);
+  next();
+});
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
@@ -16,31 +22,33 @@ app.get("/json", function(req, res) {
   res.json(json);
 });
 
-// --> 7)  Mount the Logger middleware here
+const middleware = (req, res, next)=> {
+  req.time = new Date().toString();
+  next();
+};
 
-// --> 11)  Mount the body-parser middleware  here
+app.get("/now", middleware, (req, res)=>{
+  res.send({
+    time: req.time
+  });
+});
 
-/** 1) Meet the node console. */
+app.get("/:word/echo", (req, res) => {
+  const { word } = req.params;
+  res.json({
+    echo: word
+  });
+});
 
-/** 2) A first working Express Server */
-
-/** 3) Serve an HTML file */
-
-/** 4) Serve static assets  */
-
-/** 5) serve JSON on a specific route */
-
-/** 6) Use the .env file to configure the app */
-
-/** 7) Root-level Middleware - A logger */
-//  place it before all the routes !
-
-/** 8) Chaining middleware. A Time server */
-
-/** 9)  Get input from client - Route parameters */
-
-/** 10) Get input from client - Query parameters */
-// /name?first=<firstname>&last=<lastname>
+app.get("/name", (req, res)=> {
+  var firstName = req.query.first;
+  var lastName = req.query.last;
+  
+  var { first: firstName, last: lastName } = req.query;
+  res.json({
+    name: `${firstName} ${lastName}`
+  });  
+});
 
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
